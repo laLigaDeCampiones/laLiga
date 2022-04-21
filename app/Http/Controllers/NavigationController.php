1301ca80d5a1d80107fa1629f4cf62ce9491eba5
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-04-21 11:48:09 +0200

    creating first routes of LaLiga

---
+++ app/Http/Controllers/NavigationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavigationController extends Controller
+{
+    public function clasification(){
+        return view('clasification');
+    }
+
+    public function register(){
+        return view('register');
+    }
+
+    public function calendar(){
+        return view('calendar');
+    }
+   
+    public function result(){
+        return view('result');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NavigationController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::controller(ClasificationController::class)->group(function(){
+//     Route::get('clasification');
+// });
+
+Route::get('/clasification', [NavigationController::class, 'clasification']);
+Route::get('/register', [NavigationController::class, 'register']);
+Route::get('/calendar', [NavigationController::class, 'calendar']);
+Route::get('/result', [NavigationController::class, 'result']);
